9df4edb60fd66f4668b159ed17b928a1ed7c7150
vasgat <vasgat@iti.gr>
2021-06-07 12:58:08 +0300

    update MiningManager -> move from GNNMiner to SwitchableMiner, diffuse to friends content-aware profile's results through the decentralized social graph using the results from the PPRMiner, add getSmoothPersonalizedProfile function to MiningManager

--- groupcommunications-api/src/main/java/eu/h2020/helios_social/modules/groupcommunications/api/mining/MiningManager.java
+++ groupcommunications-api/src/main/java/eu/h2020/helios_social/modules/groupcommunications/api/mining/MiningManager.java
@@ -1,13 +1,14 @@
 package eu.h2020.helios_social.modules.groupcommunications.api.mining;
 
 import java.util.HashMap;
+import java.util.List;
 
 import eu.h2020.helios_social.core.contextualegonetwork.Node;
 import eu.h2020.helios_social.modules.socialgraphmining.SocialGraphMiner;
 
 public interface MiningManager {
 
-    SocialGraphMiner getSocialGraphMiner();
-
     HashMap<Node, Double> getNextInteractionRecommendations(String egoContextId);
+
+    List getSmoothPersonalizedProfile(ContentAwareProfilingType profilingType);
 }
