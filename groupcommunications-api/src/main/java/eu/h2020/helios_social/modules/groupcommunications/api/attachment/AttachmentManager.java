2bb9e98ed1147e80c1935f8815c3ce1ba6011e35
vasgat <vasgat@iti.gr>
2021-06-07 12:30:23 +0300

    Updates on AttachmentManager (add validateAttachments and storeOutgoingAttachmentsToExternalStorage methods and update uploadAttachments method)

--- groupcommunications-api/src/main/java/eu/h2020/helios_social/modules/groupcommunications/api/attachment/AttachmentManager.java
+++ groupcommunications-api/src/main/java/eu/h2020/helios_social/modules/groupcommunications/api/attachment/AttachmentManager.java
@@ -1,10 +1,16 @@
 package eu.h2020.helios_social.modules.groupcommunications.api.attachment;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 import eu.h2020.helios_social.modules.groupcommunications.api.messaging.Attachment;
 
 public interface AttachmentManager {
+
+    boolean validateAttachments(List<Attachment> attachments);
+
+    void storeOutgoingAttachmentsToExternalStorage(List<Attachment> attachments);
+
     void uploadAttachments(List<Attachment> attachments);
 
     void downloadAttachments(String messageId, List<Attachment> attachments);
